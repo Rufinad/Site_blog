78c602069a0b15ffa74be581a66bdf29bdc46fb4
Rufinad <alsergrits@gmail.com>
2023-06-11 23:12:44 +0300

    model for saving images

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "social_django",
     "django_extensions",
-    "images.apps.ImagesConfig",
+    'images.apps.ImagesConfig'
 ]
 
 MIDDLEWARE = [
